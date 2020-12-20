d42af94244ce7210e8b0d3f7a36b80351b31db49
Andrewwxo <andrewwxo@gmail.com>
2020-12-20 12:29:05 +0300

    ignore something

--- Second_task.py
+++ Second_task.py
@@ -1,17 +1,26 @@
-def square(*args):
-    s = []
+def number_filter(*args):
+    prost = []
+    chet = []
+    nechet = []
+    n = 0
     for i in args:
-        s.append(i ** 2)
-    print(s)
+        for j in range(2, i):
+            if i % j == 0:
+                n += 1
+        if n == 0:
+            prost.append(i)
+        else:
+            n = 0
 
-square(2, 3 ,4)
+    print("Простые числа: ", prost)
 
-
-keyword = int(input("Введите степень: "))
-
-def square_2(*args):
-    s = []
     for i in args:
-       s.append(i ** keyword)
-    print(s)
-square_2(2, 3, 4)
+        if i%2 == 0:
+            chet.append(i)
+
+        else:
+            nechet.append(i)
+    print("Четные: ", chet, "Нечетные: ", nechet)
+
+
+number_filter(2,3,4,5,6,7, 13, 23, 8)
