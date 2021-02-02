33ef0d894ea9e9c42b33dcaaa66d27aa5e441213
Andrewwxo <andrewwxo@gmail.com>
2021-02-02 21:23:46 +0300

    Created main module

--- Task_3_Classes.py
+++ Task_3_Classes.py
@@ -1,44 +1,51 @@
 class Vehicle:
     type_name = None
 
-    def __init__(self, name, weight, carrying_capacity):
+    def __init__(self, name, weight, carrying_capacity, engine_starting_password):
         self.name = name
         self.weight = weight
         self.carrying_capacity = carrying_capacity
+        self.engine_starting_password = engine_starting_password
 
     def __str__(self):
-        return f'{self.name} ({self.type_name})'
+        return f'{self.name}: Вес, кг: {self.weight}; ' \
+               f'Грузоподъемность: {self.carrying_capacity}; ' \
+               f'Пароль для запуска двигателя: {self.engine_starting_password}; ({self.type_name})'
 
-    def sound(self):
-        print('sound on')
+    @property
+    def engine_starting_password_1(self):
+        return self.engine_starting_password
+
+    @engine_starting_password_1.setter
+    def engine_starting_password_1(self, val):
+        try:
+            int(val)
+        except Exception:
+            raise ValueError('Пароль должен быть числом')
+        self.engine_starting_password = val
+
 
 class Ship(Vehicle):
     type_name = 'Яхты'
 
-    def __init__(self, name, weight, carrying_capacity, displacement):
-        super().__init__(name, weight, carrying_capacity)
+    def __init__(self, name, weight, carrying_capacity, engine_starting_password , displacement):
+        super().__init__(name, weight, carrying_capacity, engine_starting_password)
         self.displacement = displacement
 
 class Car(Vehicle):
     type_name = 'Легковые авто'
 
-    def __init__(self, name, weight, carrying_capacity, number_of_wheels):
-        super().__init__(name, weight, carrying_capacity)
+    def __init__(self, name, weight, carrying_capacity, engine_starting_password, number_of_wheels):
+        super().__init__(name, weight, carrying_capacity, engine_starting_password)
         self.number_of_wheels = number_of_wheels
 
 class Plan(Vehicle):
     type_name = 'Самолеты'
 
-    def __init__(self, name, weight, carrying_capacity, limit_height):
-        super().__init__(name, weight, carrying_capacity)
+    def __init__(self, name, weight, carrying_capacity, engine_starting_password, limit_height):
+        super().__init__(name, weight, carrying_capacity, engine_starting_password)
         self.limit_height = limit_height
 
-ship_1 = Ship('Yacht Eclipse', 10000, 20000, 13000 )
-car_1 = Car('Kia Optima', 1200, 2200, 4)
-plan_1 = Plan('An - 2M', 3620, 1500, 4100)
 
-print(ship_1)
-print(car_1)
-print(plan_1)
 
-print(ship_1.weight)
+
