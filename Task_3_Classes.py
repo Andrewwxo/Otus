ed63eccce4a5814a11d9f594d87c21b2bc581000
Andrewwxo <andrewwxo@gmail.com>
2021-02-05 15:13:56 +0300

    Created depreciation

--- Task_3_Classes.py
+++ Task_3_Classes.py
@@ -1,6 +1,7 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
+from datetime import datetime
 
-class Vehicle():
+class Vehicle(ABC):
     type_name = None
 
     def __init__(self, name, weight, carrying_capacity, engine_starting_password):
@@ -12,7 +13,9 @@ class Vehicle():
     def __str__(self):
         return f'{self.name}: Вес, кг: {self.weight}; ' \
                f'Грузоподъемность: {self.carrying_capacity}; ' \
-               f'Пароль для запуска двигателя: {self.engine_starting_password}; ({self.type_name})'
+               f'Пароль для запуска двигателя: {self.engine_starting_password}; ' \
+               f'Аммортизация: {self.depreciation}' \
+               f'({self.type_name})'
 
     @property
     @abstractmethod
@@ -27,6 +30,14 @@ class Vehicle():
             raise ValueError('Пароль должен быть числом')
         self.engine_starting_password = val
 
+    @property
+    @abstractmethod
+    def depreciation(self):
+        current_year = datetime.now().year
+        date_of_issue = 2015  # Рассчитываю аммортизацию для транспорта 2015 года выпуска
+        coefficient_depreciation = 0.05
+        depreciation = (current_year - date_of_issue) * coefficient_depreciation
+        return depreciation
 
 class Ship(Vehicle):
     type_name = 'Яхты'
@@ -35,12 +46,24 @@ class Ship(Vehicle):
         super().__init__(name, weight, carrying_capacity, engine_starting_password)
         self.displacement = displacement
 
+    def engine_starting_password_1(self):
+        return self
+
+    def depreciation(self):
+        return self
+
 class Car(Vehicle):
     type_name = 'Легковые авто'
 
     def __init__(self, name, weight, carrying_capacity, engine_starting_password, number_of_wheels):
         super().__init__(name, weight, carrying_capacity, engine_starting_password)
         self.number_of_wheels = number_of_wheels
+
+    def engine_starting_password_1(self):
+        return self
+
+    def depreciation(self):
+        return self
 
 class Plan(Vehicle):
     type_name = 'Самолеты'
@@ -49,6 +72,10 @@ class Plan(Vehicle):
         super().__init__(name, weight, carrying_capacity, engine_starting_password)
         self.limit_height = limit_height
 
+    def engine_starting_password_1(self):
+        return self
 
+    def depreciation(self):
+        return self
 
 
