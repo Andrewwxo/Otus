0f868934b1ff3a9dc8277c531e70489dd7fe850a
Andrewwxo <andrewwxo@gmail.com>
2021-02-06 14:54:43 +0300

    Add metaclass

--- Task_3_Classes.py
+++ Task_3_Classes.py
@@ -1,7 +1,16 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
+from dataclasses import dataclass
 
-class Vehicle(ABC):
+
+class VehicleMeta(type):
+    def __new__(mcs, name, bases, attrs):
+        new_class = super().__new__(mcs, name, bases, attrs)
+        if 'year_of_production' not in attrs:
+            new_class.year_of_production = None
+        return new_class
+
+class Vehicle(metaclass=VehicleMeta):
     type_name = None
 
     def __init__(self, name, weight, carrying_capacity, engine_starting_password):
@@ -14,7 +23,6 @@ class Vehicle(ABC):
         return f'{self.name}: Вес, кг: {self.weight}; ' \
                f'Грузоподъемность: {self.carrying_capacity}; ' \
                f'Пароль для запуска двигателя: {self.engine_starting_password}; ' \
-               f'Аммортизация: {self.depreciation}' \
                f'({self.type_name})'
 
     @property
@@ -30,14 +38,7 @@ class Vehicle(ABC):
             raise ValueError('Пароль должен быть числом')
         self.engine_starting_password = val
 
-    @property
-    @abstractmethod
-    def depreciation(self):
-        current_year = datetime.now().year
-        date_of_issue = 2015  # Рассчитываю аммортизацию для транспорта 2015 года выпуска
-        coefficient_depreciation = 0.05
-        depreciation = (current_year - date_of_issue) * coefficient_depreciation
-        return depreciation
+
 
 class Ship(Vehicle):
     type_name = 'Яхты'
@@ -49,8 +50,6 @@ class Ship(Vehicle):
     def engine_starting_password_1(self):
         return self
 
-    def depreciation(self):
-        return self
 
 class Car(Vehicle):
     type_name = 'Легковые авто'
@@ -59,23 +58,40 @@ class Car(Vehicle):
         super().__init__(name, weight, carrying_capacity, engine_starting_password)
         self.number_of_wheels = number_of_wheels
 
+    def __str__(self):
+        return f'{self.name}: Вес, кг: {self.weight}; ' \
+               f'Грузоподъемность: {self.carrying_capacity}; ' \
+               f'Пароль для запуска двигателя: {self.engine_starting_password}; ' \
+               f'Аммортизация: {self.depreciation}' \
+               f'({self.type_name})'
+
     def engine_starting_password_1(self):
         return self
 
+    @abstractmethod
     def depreciation(self):
-        return self
+        current_year = datetime.now().year
+        year_of_production = 2015  # Рассчитываю аммортизацию для транспорта 2015 года выпуска
+        coefficient_depreciation = 0.05
+        depreciation = (current_year - year_of_production) * coefficient_depreciation
+        return depreciation
 
+
+@dataclass
 class Plan(Vehicle):
     type_name = 'Самолеты'
 
-    def __init__(self, name, weight, carrying_capacity, engine_starting_password, limit_height):
-        super().__init__(name, weight, carrying_capacity, engine_starting_password)
-        self.limit_height = limit_height
+    name: str
+    weight: int
+    carrying_capacity: int
+    engine_starting_password: int
+    limit_height: int
+
 
     def engine_starting_password_1(self):
         return self
 
-    def depreciation(self):
-        return self
+
+
 
 
